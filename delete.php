32878ec4c87f9fe84263973777848babc8803edb
ChernovVV <chernov@intervolga.ru>
2021-08-12 10:07:28 +0300

    delete
    удаление информации из БД, HTML код экранируется, работа с БД производится через ПДО, ошибки при выполнении кода отслеживаются

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'connect.php';
+
+$id = $_GET['id'];
+
+$id = htmlspecialchars($id);
+
+try {
+
+	$sql = $pdo->prepare("DELETE FROM Country WHERE id=:countryId"); //подготавливаем запрос
+	$params = ['countryId' => $id]; //подставляем переменные в маску
+	$sql->execute($params); //соеденяем переменные с маской
+
+} catch (PDOException $e) { //отслеживание ошибок кода вставки данных в БД
+
+    die($e->getMessage());	//вывод сообщения ошибки
+}
+header('Location: /');
