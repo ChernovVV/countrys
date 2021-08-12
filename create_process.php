3b382a1e0c3cca0446182591cd292ea00e90774e
ChernovVV <chernov@intervolga.ru>
2021-08-12 10:06:20 +0300

    create_process
    добавление данных в БД с экранированием HTML тегов, данные отправляются через ПДО, ошибки отслеживаются

---
+++ create_process.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'connect.php';
+
+$Country = $_POST['Country']; //принимает переменные с формы
+$Capital = $_POST['Capital'];
+$Population = $_POST['Population'];
+$Volume = $_POST['Volume'];
+
+$Country = htmlspecialchars($Country); //экранируем html теги
+$Capital = htmlspecialchars($Capital);
+$Population = htmlspecialchars($Population);
+$Volume = htmlspecialchars($Volume);
+
+try {
+
+    $sql = $pdo->prepare ("INSERT INTO Country SET Country=:country, Capital=:capital, Population=:population, Volume=:volume"); //подготавливаем запрос
+    $params = ['country' => $Country, 'capital' => $Capital, 'population' => $Population, 'volume' => $Volume]; //подставляем переменные в маску
+    $sql->execute($params); //соеденяем переменные с маской
+    
+} catch (PDOException $e) { //отслеживание ошибок кода вставки данных в БД
+
+    die($e->getMessage()); //вывод сообщения ошибки
+}
+
+header('location: / ');
+?>
