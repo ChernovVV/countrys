1c81430d7703a198d2ccf484ba4c78262e59e001
ChernovVV <88819838+ChernovVV@users.noreply.github.com>
2021-08-15 18:08:19 +0300

    connect
    подключение к БД через ПДО с отслеживанием ошибок

--- connect.php
+++ connect.php
@@ -6,14 +6,15 @@ $user = 'x937731t_test';
 $pass = 'Magirus987';
 
 $dsn = "mysql:host=$host;dbname=$database;";
- try { $pdo = new PDO($dsn, $user, $pass);
-  
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try { 
+	$pdo = new PDO($dsn, $user, $pass);
 
- } catch (PDOException $e) {
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
- 	die($e->getMessage()); //вывод сообщения ошибки
+} catch (PDOException $e) {
 
- }
+	die($e->getMessage()); //вывод сообщения ошибки
+
+}
 
 ?>
