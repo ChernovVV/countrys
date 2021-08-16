80305a2c380f4374895f2e5a9e7708c7a0215c28
ChernovVV <chernov@intervolga.ru>
2021-08-16 09:34:14 +0300

    update
    страница обновления данных на сайте, построена на bootstrap. При переходе на эту страницу в полях указываются данные, которые установлены в БД в настоящий момент, что позволяет удобнее корректировать данные. Процесс обновления данных в БД производится в другом файле — update_process.php

--- update.php
+++ update.php
@@ -1,56 +1,38 @@
 <?php
+
 require_once 'connect.php';
-
-$id = $_GET['id'];
-
-$id = htmlspecialchars($id); //экранируем HTML теги
-
-try {
-
-	$coutrys = $pdo->prepare('SELECT * FROM `Country` WHERE `id`=:id LIMIT 1'); //подготавливаем запрос
-	$params = ['id' => $id]; //подставляем переменные в маску
-	$coutrys->execute($params); //соеденяем переменные с маской
-	$coutry = $coutrys->fetch(PDO::FETCH_ASSOC); //сортируем массив
-
-} catch (PDOException $e) { //отслеживание ошибок кода вставки данных в БД
-
-    die($e->getMessage());	//вывод сообщения ошибки
-}
+require_once 'pluggabledata.php';
+require_once 'htmlsample.php';
 
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Обновление данных стран</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles.css">
+<meta charset="UTF-8">
+	<title>Обновление данных стран</title>
 </head>
 <body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="script.js"></script>
-        <h4>Обновление данных стран</h4>
-        <form action="update_process.php" method="post">
-          <input type="hidden" name="id" value="<?= $coutry['id'] ?>">
-          <div class="mb-3">
-            <label class="form-label">Страна</label>
-            <input type="text" class="form-control" name="Country" value="<?= $coutry['Country'] ?>">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Столица</label>
-            <input type="text" class="form-control" name="Capital" value="<?= $coutry['Capital'] ?>">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Население</label>
-            <input type="number" class="form-control" name="Population" value="<?= $coutry['Population'] ?>">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Площать страны</label>
-            <input type="number" class="form-control" name="Volume" value="<?= $coutry['Volume'] ?>">
-          </div>
-            <button type="submit" class="btn btn-primary" onclick="press_update()">Обновить данные страны</button>
-          </div>
-        </form>
+	<h4>Обновление данных стран</h4>
+	<form action="update_process.php" method="post">
+		<input type="hidden" name="id" value="<?= $coutry_for_update['id'] ?>">
+	<div class="mb-3">
+		<label class="form-label">Страна</label>
+		<input type="text" class="form-control" name="Country" value="<?= htmlspecialchars($coutry_for_update['Country']) ?>"  required>
+	</div>
+	<div class="mb-3">
+		<label class="form-label">Столица</label>
+		<input type="text" class="form-control" name="Capital" value="<?= htmlspecialchars($coutry_for_update['Capital']) ?>"  required>
+	</div>
+	<div class="mb-3">
+		<label class="form-label">Население</label>
+		<input type="number" class="form-control" name="Population" value="<?= $coutry_for_update['Population'] ?>"  required>
+	</div>
+	<div class="mb-3">
+		<label class="form-label">Площать страны</label>
+		<input type="number" class="form-control" name="Volume" value="<?= $coutry_for_update['Volume'] ?>"  required>
+	</div>
+		<button type="submit" class="btn btn-primary" onclick="press_update()">Обновить данные страны</button>
+	</form>
 </body>
 </html>
