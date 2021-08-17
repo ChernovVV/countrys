6cf85bdf35bfd2ea481ccc10c7b46c122990d579
ChernovVV <88819838+ChernovVV@users.noreply.github.com>
2021-08-17 14:51:13 +0300

    update
    страница обновления данных на сайте, построена на bootstrap. При переходе на эту страницу в полях указываются данные, которые установлены в БД в настоящий момент, что позволяет удобнее корректировать данные. Процесс обновления данных в БД производится в другом файле — update_process.php

--- update.php
+++ update.php
@@ -15,22 +15,22 @@ require_once 'htmlsample.php';
 <body>
 	<h4>Обновление данных стран</h4>
 	<form action="update_process.php" method="post">
-		<input type="hidden" name="id" value="<?= $coutry_for_update['id'] ?>">
+		<input type="hidden" name="id" value="<?= $coutrys_function_update['id'] ?>">
 	<div class="mb-3">
 		<label class="form-label">Страна</label>
-		<input type="text" class="form-control" name="Country" value="<?= htmlspecialchars($coutry_for_update['Country']) ?>"  required>
+		<input type="text" class="form-control" name="Country" value="<?= htmlspecialchars($coutrys_function_update['Country']) ?>"  required>
 	</div>
 	<div class="mb-3">
 		<label class="form-label">Столица</label>
-		<input type="text" class="form-control" name="Capital" value="<?= htmlspecialchars($coutry_for_update['Capital']) ?>"  required>
+		<input type="text" class="form-control" name="Capital" value="<?= htmlspecialchars($coutrys_function_update['Capital']) ?>"  required>
 	</div>
 	<div class="mb-3">
 		<label class="form-label">Население</label>
-		<input type="number" class="form-control" name="Population" value="<?= $coutry_for_update['Population'] ?>"  required>
+		<input type="number" class="form-control" name="Population" value="<?= $coutrys_function_update['Population'] ?>"  required>
 	</div>
 	<div class="mb-3">
 		<label class="form-label">Площать страны</label>
-		<input type="number" class="form-control" name="Volume" value="<?= $coutry_for_update['Volume'] ?>"  required>
+		<input type="number" class="form-control" name="Volume" value="<?= $coutrys_function_update['Volume'] ?>"  required>
 	</div>
 		<button type="submit" class="btn btn-primary" onclick="press_update()">Обновить данные страны</button>
 	</form>
